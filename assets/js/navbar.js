67c834b4917eadeadba377043dceb26d1e9d24da
Carlos Pinheiro <pinheirosilvacarlos@gmail.com>
2023-03-17 19:32:14 -0300

    Finished - Solving hamburguer menu & adding logo

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,12 +1,21 @@
-const hamburguer = document.querySelector(".hamburguer");
-const navMenu = document.querySelector(".nav-menu");
+(function() {
+    const navbar = document.querySelector('.navbar');
+    const button = navbar.querySelector('.menu');
+    const nav = navbar.querySelector('.navlinks');
+    const navLinks = nav.querySelectorAll('li a');
 
-hamburguer.addEventListener("click", () => {
-    hamburguer.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+    button.onclick = () => {
+        navbar.classList.toggle('active');
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener ("click", () => {
-    hamburguer.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
+        const expanded = nav.getAttribute('aria-expanded');
+        nav.setAttribute('aria-expanded', expanded === 'false');
+    };
+
+    const forEach = Array.prototype.forEach;
+    forEach.call(navLinks, (link) => {
+        link.onclick = () => {
+            navbar.classList.remove('active');
+            nav.setAttribute('aria-expanded', 'false');
+        };
+    });
+})();
